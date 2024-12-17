f1cd62b22cef3f50de6c953eb8ee4a69d25e8833
Sangram Thapa <thapasangram64@gmail.com>
2024-12-17 19:56:26 +0545

    ui of calc

--- calculator.js
+++ calculator.js
@@ -1,21 +1,21 @@
-function calculator(calc){
-   const value1= parseInt(document.getElementById("FirstNumber").value);
+function calculator(calc) {
+    const value1 = parseInt(document.getElementById("FirstNumber").value);
     const value2 = parseInt(document.getElementById("SecondNumber").value);
     const r = document.getElementById("result");
 
-    if(calc === "add"){
-        r.innerHTML = value1 + value2 ;
+    if (calc === "add") {
+        r.innerHTML = value1 + value2;
     }
-    else if(calc === "sub"){
-        r.innerHTML = value1 - value2 ;
+    else if (calc === "sub") {
+        r.innerHTML = value1 - value2;
     }
-    else if(calc === "mul"){
-        r.innerHTML = value1 * value2 ;
+    else if (calc === "mul") {
+        r.innerHTML = value1 * value2;
     }
-    else if(calc === "div"){
-        r.innerHTML = value1 / value2 ;
+    else if (calc === "div") {
+        r.innerHTML = value1 / value2;
     }
-    else{
+    else {
         alert("Invalid Operation");
     }
 }
