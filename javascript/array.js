938fa92ecfb3eecdadc9a0b7af9d911961362c97
Sangram Thapa <thapasangram64@gmail.com>
2024-12-18 16:51:37 +0545

    practice

--- javascript/array.js
+++ javascript/array.js
@@ -1,24 +1,11 @@
-function demo() {
-    const names = ["Ram", "Shyam", "Hari", "Gita"];
-    names.forEach(element => {
-        const node1 = document.createElement("li");
-        const textnode1 = document.createTextNode(element); 
-        node1.appendChild(textnode1);
-    
-        document.getElementById("ullist").appendChild(node1);
+
+function names() {
+    const a = ["ram", "shyam", "hari"];
+    a.forEach(element => {
+        const x = document.createElement("li");
+        const y = document.createTextNode(element);
+        x.appendChild(y);
+        document.getElementById("listofnames").appendChild(x);
     });
+
 }
-
-
-// const node2 = document.createElement("li");
-// const textnode2 = document.createTextNode("BIjen");
-// node2.appendChild(textnode2);
-
-
-// const node3 = document.createElement("li");
-// const textnode3 = document.createTextNode("MIxo");
-// node3.appendChild(textnode3);
-
-
-// document.getElementById("ullist").appendChild(node1);
-// document.getElementById("ullist").appendChild(node2);
