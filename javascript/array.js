49de5a61139afda8e28d28afcdd302db8445ce53
Sangram Thapa <thapasangram64@gmail.com>
2024-12-18 13:34:00 +0545

    array concept

--- javascript/array.js
+++ javascript/array.js
@@ -1,20 +1,24 @@
 function demo() {
-    const node1 = document.createElement("li");
-    const textnode1 = document.createTextNode("RAJAN");
-    node1.appendChild(textnode1);
-
-
-    const node2 = document.createElement("li");
-    const textnode2 = document.createTextNode("BIjen");
-    node2.appendChild(textnode2);
-
-   
-    const node3 = document.createElement("li");
-    const textnode3 = document.createTextNode("MIxo");
-    node3.appendChild(textnode3);
-
-   
-    document.getElementById("ullist").appendChild(node1);
-    document.getElementById("ullist").appendChild(node2);
-    document.getElementById("ullist").appendChild(node3);
+    const names = ["Ram", "Shyam", "Hari", "Gita"];
+    names.forEach(element => {
+        const node1 = document.createElement("li");
+        const textnode1 = document.createTextNode(element); 
+        node1.appendChild(textnode1);
+    
+        document.getElementById("ullist").appendChild(node1);
+    });
 }
+
+
+// const node2 = document.createElement("li");
+// const textnode2 = document.createTextNode("BIjen");
+// node2.appendChild(textnode2);
+
+
+// const node3 = document.createElement("li");
+// const textnode3 = document.createTextNode("MIxo");
+// node3.appendChild(textnode3);
+
+
+// document.getElementById("ullist").appendChild(node1);
+// document.getElementById("ullist").appendChild(node2);
